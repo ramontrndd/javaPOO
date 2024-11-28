7fe7d4c5bfd1a23d6d434742470966f782855a54
ramontrndd <134494678+ramontrndd@users.noreply.github.com>
2024-11-28 00:39:47 -0300

    feat: add get and set method

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -2,9 +2,9 @@ package entities;
 
 public class Product {
 
-    public String name;
-    public double price;
-    public int quantity;
+    private String name;
+    private double price;
+    private int quantity;
 
     public Product(String name, double price, int quantity) {
         this.name = name;
@@ -22,6 +22,22 @@ public class Product {
     public void removeProducts(int quantity) {
         this.quantity -= quantity;
     }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+
 
     public String toString() {
         return name

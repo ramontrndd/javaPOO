7967691fa9dae00822668a560b5cd9ba21f5bed2
ramontrndd <134494678+ramontrndd@users.noreply.github.com>
2024-11-29 12:11:12 -0300

    feat: exerc enums

---
+++ src/entities/Departament.java
@@ -0,0 +1,17 @@
+package entities;
+
+public class Departament {
+    private String Name;
+
+    public Departament(String name) {
+        Name = name;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
